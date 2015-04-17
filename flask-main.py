2265a9994758306f6a6ea3f59d239a442fe7365e
Romain Gisiger <rom_30@hotmail.fr>
2015-04-17 11:15:54 +0200

    Update logging

--- flask-main.py
+++ flask-main.py
@@ -316,13 +316,13 @@ def configuration():
 def log():
     return 1/0
 
-from logging import FileHandler, Formatter
+from logging import FileHandler, Formatter, DEBUG
 
 if __name__ == '__main__':
     try:
         backend.start()
     	file_handler = FileHandler("flask.log")
-    	file_handler.setLevel(logging.DEBUG)
+    	file_handler.setLevel(DEBUG)
     	file_handler.setFormatter(Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
     	app.logger.addHandler(file_handler)
 
